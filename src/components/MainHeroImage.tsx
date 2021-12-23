24b39cefd643d3726c91085569494f148d2e8510
orzmeta <orzmeta@protonmail.com>
2021-12-23 13:52:18 +0800

    fix: fuck js

--- src/components/MainHeroImage.tsx
+++ src/components/MainHeroImage.tsx
@@ -8,8 +8,8 @@ const MainHeroImage = () => {
     <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
       <img
         className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-        src={mainHero.img}
-        alt="happy team image"
+        src={mainHero.file}
+        alt="happy team "
       />
     </div>
   );
